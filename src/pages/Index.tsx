38b65699ae6794f91c1c3ee14c7fe7b1e4da8180
Himeth Randiv Dharmapriya <randivdharmapriya88@gmail.com>
2025-05-31 12:08:11 +0530

    push

--- src/pages/Index.tsx
+++
@@ -1,7 +0,0 @@
-
-// This file is redundant now since we created the Home page
-// We'll just redirect to Home component for backward compatibility
-
-import Home from "./Home";
-
-export default Home;
