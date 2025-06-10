ade3c847a228a7655db9678cca58de7377863687
Himeth Randiv Dharmapriya <randivdharmapriya88@gmail.com>
2025-06-10 15:48:18 +0530

    tarw

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { SpeedInsights } from '@vercel/speed-insights/react';
+import { Analytics } from '@vercel/analytics/react';
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import WhyAI from "./pages/WhyAI";
@@ -16,6 +18,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
+      <SpeedInsights />
+      <Analytics />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
